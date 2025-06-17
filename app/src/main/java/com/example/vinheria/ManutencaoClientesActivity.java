9f1dac28f60d9b9d54db8e41cfea8fccef3efada
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-06-16 23:53:35 -0300

    Update ManutencaoClientesActivity.java

--- app/src/main/java/com/example/vinheria/ManutencaoClientesActivity.java
+++ app/src/main/java/com/example/vinheria/ManutencaoClientesActivity.java
@@ -1,12 +1,24 @@
 package com.example.vinheria;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ManutencaoClientesActivity extends AppCompatActivity {
+
+    private Button btnClientesCadastrados;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manutencaoclientes);
+
+        btnClientesCadastrados = findViewById(R.id.clientesCadastrados);
+
+        btnClientesCadastrados.setOnClickListener(v -> {
+            Intent intent = new Intent(ManutencaoClientesActivity.this, ClientesCadastradosActivity.class);
+            startActivity(intent);
+        });
     }
 }
