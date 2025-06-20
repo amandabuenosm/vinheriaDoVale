3b88f6ab2b9b325f033be72cb217bafa0fac4cc7
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-06-20 00:19:31 -0300

    Update backend in ClientDAO.java

--- app/src/main/java/com/example/vinheria/dao/ClientDAO.java
+++ app/src/main/java/com/example/vinheria/dao/ClientDAO.java
@@ -24,7 +24,14 @@ public class ClientDAO {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues valores = new ContentValues();
         valores.put("nome", cliente.getNome());
+        valores.put("cpf_cnpj", cliente.getCpf_cnpj());
+        valores.put("telefone", cliente.getTelefone());
         valores.put("email", cliente.getEmail());
+        valores.put("endereco", cliente.getEndereco());
+        valores.put("cidade", cliente.getCidade());
+        valores.put("estado", cliente.getEstado());
+        valores.put("cep", cliente.getCep());
+        valores.put("observacoes", cliente.getObservacoes());
         db.insert("clientes", null, valores);
         db.close();
     }
@@ -35,12 +42,19 @@ public class ClientDAO {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM clientes", null);
 
-        if(cursor.moveToFirst()) {
+        if (cursor.moveToFirst()) {
             do {
                 Client cliente = new Client();
                 cliente.setId(cursor.getInt(cursor.getColumnIndexOrThrow("id")));
                 cliente.setNome(cursor.getString(cursor.getColumnIndexOrThrow("nome")));
+                cliente.setCpf_cnpj(cursor.getString(cursor.getColumnIndexOrThrow("cpf_cnpj")));
+                cliente.setTelefone(cursor.getString(cursor.getColumnIndexOrThrow("telefone")));
                 cliente.setEmail(cursor.getString(cursor.getColumnIndexOrThrow("email")));
+                cliente.setEndereco(cursor.getString(cursor.getColumnIndexOrThrow("endereco")));
+                cliente.setCidade(cursor.getString(cursor.getColumnIndexOrThrow("cidade")));
+                cliente.setEstado(cursor.getString(cursor.getColumnIndexOrThrow("estado")));
+                cliente.setCep(cursor.getString(cursor.getColumnIndexOrThrow("cep")));
+                cliente.setObservacoes(cursor.getString(cursor.getColumnIndexOrThrow("observacoes")));
                 lista.add(cliente);
             } while (cursor.moveToNext());
         }
