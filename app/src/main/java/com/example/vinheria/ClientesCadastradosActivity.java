bb48982dc5d9ccbb3560fc17933ee3a24292047f
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2025-06-20 00:22:07 -0300

    Update ClientesCadastradosActivity.java

--- app/src/main/java/com/example/vinheria/ClientesCadastradosActivity.java
+++ app/src/main/java/com/example/vinheria/ClientesCadastradosActivity.java
@@ -6,9 +6,11 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.vinheria.adapters.ClientAdapter;
 import com.example.vinheria.dao.ClientDAO;
 import com.example.vinheria.models.Client;
+
 import java.util.List;
 
 public class ClientesCadastradosActivity extends AppCompatActivity {
@@ -29,10 +31,41 @@ public class ClientesCadastradosActivity extends AppCompatActivity {
 
         // inserção no banco para testes
         if (clienteDAO.listarClientes().isEmpty()) {
-            Client c1 = new Client(0, "Amanda", "amanda@email.com");
-            Client c2 = new Client(0, "Bruno", "bruno@email.com");
+            Client c1 = new Client();
+            c1.setNome("Raimundo Tiago Caldeira");
+            c1.setCpf_cnpj("556.445.956-33");
+            c1.setTelefone("11 99805-2261");
+            c1.setEmail("raimundo_caldeira@fibran.com.br");
+            c1.setEndereco("Rua Rinópolis, 623, Bairro Cidade Industrial Satélite de São Paulo");
+            c1.setCidade("São Paulo");
+            c1.setEstado("SP");
+            c1.setCep("69918-178");
+            c1.setObservacoes("Cliente VIP");
             clienteDAO.inserirCliente(c1);
+
+            Client c2 = new Client();
+            c2.setNome("Vinícola Trevisol LTDA");
+            c2.setCpf_cnpj("47.085.221/0001-73");
+            c2.setTelefone("48 3940-2249");
+            c2.setEmail("administrativo@trevisol.com.br");
+            c2.setEndereco("Rua Arcelino Pedro Batista, 521, São Simão");
+            c2.setCidade("Criciúma");
+            c2.setEstado("SC");
+            c2.setCep("88811-830");
+            c2.setObservacoes("Parente do dono");
             clienteDAO.inserirCliente(c2);
+
+            Client c3 = new Client();
+            c3.setNome("Isabelly Eliane Laura da Cunha");
+            c3.setCpf_cnpj("811.095.219-44");
+            c3.setTelefone("41 98766-2660");
+            c3.setEmail("isabelly-dacunha73@db9.com.br");
+            c3.setEndereco("Rua Antonio dos Santos, 849, Ipê");
+            c3.setCidade("São José dos Pinhais");
+            c3.setEstado("PR");
+            c3.setCep("83055-519");
+            c3.setObservacoes("Nova cliente");
+            clienteDAO.inserirCliente(c3);
         }
 
         // carga de clientes cadastrados
